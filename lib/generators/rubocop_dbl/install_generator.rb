ce5752b4441b893a53da1349127c39ead317cf12
Lud <lud@reinmiedl.com>
2024-03-19 12:56:34 +0100

    fix version 2

--- lib/generators/rubocop_dbl/install_generator.rb
+++ lib/generators/rubocop_dbl/install_generator.rb
@@ -11,6 +11,7 @@ module RubocopDbl
 
       def create_config_file
         file_method = config_file_exists? ? :prepend : :create
+        content = defined?(Rails) ? config_file_content_rails : config_file_content
         send :"#{file_method}_file", config_file_path, config_file_content
       end
 
@@ -29,6 +30,120 @@ module RubocopDbl
           inherit_gem:
             rubocop-dbl:
               - config/dbl.yml
+
+          # we do not want to overwrite the base-array (from rubocop-dbl), but extend it
+          inherit_mode:
+            merge:
+              - Exclude
+
+          AllCops:
+            TargetRubyVersion: 3.3.0
+            Exclude:
+              - 'sorbet/rbi/shims/**/*'
+        HEREDOC
+      end
+
+      def config_file_content
+        <<~HEREDOC
+          require:
+            - rubocop-rails
+
+          inherit_gem:
+            rubocop-dbl:
+              - config/dbl.yml
+
+          inherit_mode:
+            merge:
+              - Exclude
+
+          AllCops:
+            TargetRubyVersion: 3.3.0
+            Exclude:
+              - 'sorbet/rbi/shims/**/*'
+
+          # Defaults for this cop are found here:
+          # https://github.com/rubocop/rubocop-rails/blob/master/config/default.yml
+          #
+          # Add customizations below.
+          #
+          # Ensure to document why we change a default by linking the corresponding PR.
+          # For example:
+          #
+          #     Rails/ActionFilter:
+          #       Enabled: false
+          #
+
+          # @NOTE: added: "staging"
+          Rails/UnknownEnv:
+            Environments:
+              - production
+              - development
+              - test
+              - staging
+
+          # @NOTE: disabled
+          Rails/HttpStatus:
+            Enabled: false
+
+          # @NOTE: disabled
+          Rails/ApplicationController:
+            Enabled: false
+
+
+          #
+          # @NOTE: all the cops ahead are "pending" in the current default config
+          #
+          Rails/ActiveRecordCallbacksOrder:
+            Enabled: true
+
+          Rails/AfterCommitOverride:
+            Enabled: true
+
+          Rails/AttributeDefaultBlockValue:
+            Enabled: true
+
+          Rails/FindById:
+            Enabled: true
+
+          Rails/Inquiry:
+            Enabled: true
+
+          Rails/MailerName:
+            Enabled: true
+
+          Rails/MatchRoute:
+            Enabled: true
+
+          Rails/NegateInclude:
+            Enabled: true
+
+          Rails/Pluck:
+            Enabled: true
+
+          Rails/PluckInWhere:
+            Enabled: true
+
+          Rails/RenderInline:
+            Enabled: true
+
+          Rails/RenderPlainText:
+            Enabled: true
+
+          Rails/ShortI18n:
+            Enabled: true
+            EnforcedStyle: aggressive
+
+          Rails/SquishedSQLHeredocs:
+            Enabled: true
+
+          Rails/WhereEquals:
+            Enabled: true
+
+          Rails/WhereExists:
+            Enabled: true
+
+          Rails/WhereNot:
+            Enabled: true
         HEREDOC
       end
     end
--- rubocop-dbl.gemspec
+++ rubocop-dbl.gemspec
@@ -18,13 +18,12 @@ Gem::Specification.new do |spec|
   ]
   spec.homepage              = 'https://github.com/dbl-works/rubocop-dbl'
   spec.license               = 'MIT'
-  spec.required_ruby_version = '>= 2.7'
+  spec.required_ruby_version = '>= 3.0'
 
   spec.add_dependency 'rubocop', '~> 1'
   spec.add_dependency 'rubocop-ast', '~> 1'
   spec.add_dependency 'rubocop-packaging', '~> 0.5'
   spec.add_dependency 'rubocop-performance', '~> 1'
-  spec.add_dependency 'rubocop-rails', '~> 2'
   spec.add_dependency 'rubocop-rspec', '~> 2'
   spec.add_dependency 'rubocop-sorbet', '~> 0.6'
 
